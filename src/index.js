3d653df8aea7db0e65c6d0dd1819a9464e32a1f7
Shristi Lohani <shristi@consoltica.com>
2019-02-22 17:42:18 +0545

    tic-tac-toe

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { configureStore } from './state/store';
+import App from './App.jsx';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { gameOperations } from './state/ducks/game';
+
+// At a later point, we can pull the state stored in local storage (or another source)
+// and use it to create the store from a previous state.
+const initialState = null;
+const store = configureStore(initialState || {});
+
+if (!initialState) {
+  // since we don't have any persisted state, we should start a new game when the game loads
+
+  // our new game operation returns an action object that we can use in the 
+  // redux store to dispatch
+  const newGame = gameOperations.newGame();
+
+  store.dispatch(newGame);
+}
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('app')
+);
